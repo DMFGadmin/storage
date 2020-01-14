5c97628b94e1c30d567bfac3bcf2b4cab2748286
joel <joelcote68@gmail.com>
2020-01-14 14:51:25 -0600

    remove bucket

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-module "storage_raw_ingest" {
-  source          = "./modules/storage"
-  project = var.service_project_1
-  name    = var.bd_raw_ingest_bucket
-
-}
+//module "storage_raw_ingest" {
+//  source          = "./modules/storage"
+//  project = var.service_project_1
+//  name    = var.bd_raw_ingest_bucket
+//
+//}
