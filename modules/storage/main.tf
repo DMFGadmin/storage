5cc465710cd165ab3065771140e14552550ea6b9
joel <joelcote68@gmail.com>
2020-01-14 15:49:30 -0600

    testing

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -1,25 +1,25 @@
-//resource "google_storage_bucket" "afrl-bucket" {
-//  name     = var.name
-//  location = "US"
-//  project = var.project
-//  storage_class = "STANDARD"
-//  lifecycle_rule {
-//    action {
-//      type = "SetStorageClass"
-//      storage_class = "Nearline"
-//    }
-//    condition {
-//      age = "360"
-//    }
-//  }
-//  lifecycle_rule {
-//    action {
-//      type = "SetStorageClass"
-//      storage_class = "ColdLine"
-//    }
-//    condition {
-//      age   =  "1500"
-//      matches_storage_class = ["NearLine"]
-//    }
-//  }
-//}
+resource "google_storage_bucket" "afrl-bucket" {
+  name     = var.name
+  location = "US"
+  project = var.project
+  storage_class = "STANDARD"
+  lifecycle_rule {
+    action {
+      type = "SetStorageClass"
+      storage_class = "Nearline"
+    }
+    condition {
+      age = "360"
+    }
+  }
+  lifecycle_rule {
+    action {
+      type = "SetStorageClass"
+      storage_class = "ColdLine"
+    }
+    condition {
+      age   =  "1500"
+      matches_storage_class = ["NearLine"]
+    }
+  }
+}
