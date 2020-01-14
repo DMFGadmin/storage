a84256ec17ac6228b95eb14450a9edbdff7345cc
joel <joelcote68@gmail.com>
2020-01-14 13:12:15 -0600

    testing buckets

--- main.tf
+++ main.tf
@@ -4,9 +4,3 @@ module "storage_raw_ingest" {
   name    = var.bd_raw_ingest_bucket
 
 }
-
-module "curated_data_storage" {
-  source          = "./modules/storage"
-  project         = var.service_project_1
-  name            = var.bd_curated_data_bucket
-}
